548638fdf074be9ab4965735c4aa259239a61eb4
Derin Durak <durakderin@gmail.com>
2021-09-26 00:25:50 +0300

    js folder added

---
+++ js/links.js
@@ -0,0 +1,59 @@
+function uneplink(){
+    window.open("https://zoom.us/j/8584914526");
+}
+
+function uniceflink(){
+    window.open("https://zoom.us/j/2536304247");
+}
+
+function uncstdlink(){
+    window.open("https://zoom.us/j/2185457590");
+}
+
+function ioclink(){
+    window.open("https://zoom.us/j/3062296025");
+}
+
+function natolink(){
+    window.open("https://zoom.us/j/2074256064");
+}
+
+function rusrevlink(){
+    window.open("https://zoom.us/j/3565469193");
+}
+
+function openinglink(){
+    window.open("https://zoom.us/j/2074256064");
+}
+
+function coffeelink(){
+    window.open("https://zoom.us/j/5266172632");
+}
+
+function drivelink(){
+    window.open("https://drive.google.com/drive/folders/1BQ04uLQ2g29I2pL7MbC9WXSkrSR5eEJL?usp=sharing");
+}
+
+function amonguslink(){
+    window.open("https://calpardo.com/docswithoutpwd/Among%20Us.zip");
+}
+
+function padletlink(){
+    window.open("https://padlet.com/emirormanoglu/8aoebtdgt8q2lbsd");
+}
+
+function netflixlink1(){
+    window.open("https://www.tele.pe/netflix/64532d525fce3c24?s=s160");
+}
+
+function netflixlink2(){
+    window.open("https://www.tele.pe/netflix/9095486bff73899b?s=s171");
+}
+
+function discordlink(){
+    window.open("https://discord.gg/nVNVUa2");
+}
+
+function journalslink(){
+    window.open("https://drive.google.com/drive/folders/1tuOe8QoPJJ8LGd9t_jPL0bYUGsagWGei");
+}
